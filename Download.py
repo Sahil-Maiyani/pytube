bd37a6cac6d2096c44695ddaab88d16f3e3bfede
sahil <sahilmaiyani@gmail.com>
2019-03-14 22:37:30 +0530

    total hours calculation implemented

--- Download.py
+++ Download.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+from pprint import pprint
+
 from pytube import Playlist
 
 
@@ -44,6 +47,37 @@ class Download:
         else:
             return -1
 
+    def info(self):
+        videos = os.listdir(self.folder_path)
+        hour, min, sec = 0, 0, 0
+        for video in videos:
+            duration = self._getlength(video)
+            hour += int(duration[0])
+            min += int(duration[1])
+            sec += float(duration[2])
+
+        def conversion(hour, min, sec):
+            if sec >= 60:
+                m, sec = divmod(sec, 60)
+                min += m
+            if min >= 60:
+                h, min = divmod(min, 60)
+                hour += h
+            return hour, min, sec
+
+        print("Collected {} hours {} minutes {.3f} seconds of data so far:)".format(hour, min, sec))
+
+    def _getlength(self, file_path):
+        result = subprocess.Popen(["ffprobe", self.folder_path + "/" + file_path],
+                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in result.stdout.readlines():
+            line = line.decode()
+            if "Duration" in line:
+                duration = line.split(',')[0].split(':')
+                break
+
+        return duration[1:]
+
 
 d = Download()
-d.start()
+d.info()
